51bc1de6a8a98cdd8ae8e5cc26e5e8e640a64271
Daniel Sasser <danielbsasser@gmail.com>
2020-02-21 00:16:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,6 +6,7 @@ class Artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable
+  include Memorable::InstanceMethods
   
   attr_accessor :name
   attr_reader :songs
